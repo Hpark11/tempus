1301eb5b400027161e5d08f697f4343645f0b557
HyunsooPark <qkrgustnrk@gmail.com>
2017-02-11 08:05:28 +0900

    Meeting Model Configuring

---
+++ tempus/Meeting.swift
@@ -0,0 +1,32 @@
+//
+//  Meeting.swift
+//  tempus
+//
+//  Created by hPark on 2017. 2. 11..
+//  Copyright © 2017년 boostcamp. All rights reserved.
+//
+
+import UIKit
+
+class Meeting : NSObject {
+    var cover_image_name: String?
+    var title: String?
+    var sub_title: String?
+    var meeting_type: String?
+    var category: String?
+    var upload_date: Date?
+    var userId: String?
+    
+    var slides: [MeetingSlide]?
+    var detail: MeetingDetail
+}
+
+class MeetingSlide : NSObject {
+    var image: String?
+    var header: String?
+    var content: String?
+}
+
+class MeetingDetail : NSObject {
+    
+}
