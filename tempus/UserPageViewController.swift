a3abeb2fed10e9ccbbdc929d900bf3e28a145540
HyunsooPark <qkrgustnrk@gmail.com>
2017-02-14 12:14:36 +0900

    Facebook SignIn completed

--- tempus/UserPageViewController.swift
+++ tempus/UserPageViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
-class UserPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewFlowLayout {
+class UserPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    let cellId = "cellId"
+    
     lazy var userProfileCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 0
@@ -20,12 +24,50 @@ class UserPageViewController: UIViewController, UICollectionViewDelegate, UIColl
         return collectionView
     }()
     
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 120, height: 40))
+        label.textAlignment = .center
+        label.text = "tempus"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    lazy var signOutButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "icon signout"), for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 24, height: 28)
+        button.addTarget(self, action: #selector(signOutButtonTapped), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+    
+    func signOutButtonTapped() {
+        do {
+            try FIRAuth.auth()?.signOut()
+            KeychainWrapper.standard.removeObject(forKey: Constants.keychainUid)
+            _ = navigationController?.popViewController(animated: true)
+        } catch {
+            let error = error as NSError
+            print(":::[HPARK] Sign Out Failure \(error) :::\n")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        setNavigationBarUI()
+        addSubViews()
+        setConstraints()
     }
 
+    fileprivate func setNavigationBarUI() {
+        navigationItem.titleView = titleLabel
+        let signOutButtonItem = UIBarButtonItem(customView: signOutButton)
+        self.navigationItem.leftBarButtonItem = signOutButtonItem
+    }
+    
     fileprivate func addSubViews() {
         view.addSubview(userProfileCollectionView)
     }
