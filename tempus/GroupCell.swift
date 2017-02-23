72dfec0a854e76ebbdb374ae4ddc340586269c32
HyunsooPark <qkrgustnrk@gmail.com>
2017-02-24 03:20:30 +0900

    Group chatting

--- tempus/GroupCell.swift
+++ tempus/GroupCell.swift
@@ -11,6 +11,41 @@ import Firebase
 
 class GroupCell: UITableViewCell {
     
+    var groupImageLeftAnchor: NSLayoutConstraint?
+    var groupImageRightAnchor: NSLayoutConstraint?
+    var overlayViewLeftAnchor: NSLayoutConstraint?
+    var overlayViewRightAnchor: NSLayoutConstraint?
+    var titleTextViewLeftAnchor: NSLayoutConstraint?
+    var titleTextViewRightAnchor: NSLayoutConstraint?
+    
+    var index: Int? {
+        didSet {
+            if let index = self.index {
+                if index % 2 == 0 {
+                    groupImageLeftAnchor?.isActive = true
+                    groupImageRightAnchor?.isActive = false
+                    overlayViewLeftAnchor?.isActive = true
+                    overlayViewRightAnchor?.isActive = false
+                    titleTextViewLeftAnchor?.isActive = false
+                    titleTextViewRightAnchor?.isActive = true
+//                    titleTextViewLeftAnchor?.constant = 0
+//                    titleTextViewRightAnchor?.constant = 0
+                    self.textLabel?.textAlignment = .right
+                } else {
+                    groupImageLeftAnchor?.isActive = false
+                    groupImageRightAnchor?.isActive = true
+                    overlayViewLeftAnchor?.isActive = false
+                    overlayViewRightAnchor?.isActive = true
+                    titleTextViewLeftAnchor?.isActive = true
+                    titleTextViewRightAnchor?.isActive = false
+//                    titleTextViewLeftAnchor?.constant = 8
+//                    titleTextViewRightAnchor?.constant = 0
+                    self.textLabel?.textAlignment = .left
+                }
+            }
+        }
+    }
+    
     var group: Group? {
         didSet {
             if let group = self.group {
@@ -84,6 +119,10 @@ class GroupCell: UITableViewCell {
         self.overlayView.layer.addSublayer(gradientLayer)
     }
     
+    func applyOverlayView(colorLeft: UIColor, colorRigth: UIColor) {
+        
+    }
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         addSubViews()
@@ -98,14 +137,26 @@ class GroupCell: UITableViewCell {
         addSubview(timeLabel)
     }
     
+    
+    
     fileprivate func setConstriants() {
-        _ = groupImageView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width * 0.72, heightConstant: 0)
+        let groupImageViewAnchors = groupImageView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width * 0.72, heightConstant: 0)
         
-        _ = overlayView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width * 0.72, heightConstant: 0)
+        groupImageLeftAnchor = groupImageViewAnchors[1]
+        groupImageRightAnchor = groupImageViewAnchors[3]
+        
+
+        let overlayViewAnchors = overlayView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width * 0.72, heightConstant: 0)
+        
+        overlayViewLeftAnchor = overlayViewAnchors[1]
+        overlayViewRightAnchor = overlayViewAnchors[3]
         
         //_ = overlayCoverView.anchor(topAnchor, left: nil, bottom: bottomAnchor, right: overlayView.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width * 0.2, heightConstant: 0)
         
-        _ = titleTextView.anchor(topAnchor, left: nil, bottom: bottomAnchor, right: rightAnchor, topConstant: 16, leftConstant: 0, bottomConstant: 0, rightConstant: 8, widthConstant: frame.width * 0.6, heightConstant: 0)
+        let titleTextViewAnchors = titleTextView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 16, leftConstant: 8, bottomConstant: 0, rightConstant: 8, widthConstant: frame.width * 0.6, heightConstant: 0)
+        
+        titleTextViewLeftAnchor = titleTextViewAnchors[1]
+        titleTextViewRightAnchor = titleTextViewAnchors[3]
     }
     
     
@@ -114,92 +165,3 @@ class GroupCell: UITableViewCell {
     }
 }
 
-
-//
-//
-//import UIKit
-//import Firebase
-//
-//class GroupCell: UITableViewCell {
-//    
-//    var message: Message? {
-//        didSet {
-//            setUserProfile()
-//            self.detailTextLabel?.text = message?.text
-//            if let seconds = message?.timestamp?.doubleValue {
-//                let dateTime = Date(timeIntervalSince1970: seconds)
-//                let formatter = DateFormatter()
-//                formatter.dateFormat = "hh:mm:ss a"
-//                timeLabel.text = formatter.string(from: dateTime)
-//            }
-//        }
-//    }
-//    
-//    struct UserData {
-//        static let profileImageSize: CGFloat = 48
-//        static let positionLabel: CGFloat = 64
-//    }
-//    
-//    
-//    let timeLabel: UILabel = {
-//        let label = UILabel()
-//        label.font = UIFont.systemFont(ofSize: 12)
-//        label.textColor = .lightGray
-//        label.textAlignment = .right
-//        return label
-//    }()
-//    
-//    let profileImageView: DownloadImageView = {
-//        let imageView = DownloadImageView()
-//        imageView.image = UIImage(named: "placeholder human")
-//        imageView.layer.cornerRadius = UserData.profileImageSize / 2
-//        imageView.layer.masksToBounds = true
-//        imageView.layer.borderColor = UIColor.darkGray.cgColor
-//        imageView.layer.borderWidth = 1.2
-//        imageView.contentMode = .scaleAspectFill
-//        return imageView
-//    }()
-//    
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//        
-//        textLabel?.frame = CGRect(x: UserData.positionLabel, y: textLabel!.frame.origin.y - 2, width: textLabel!.frame.width, height: textLabel!.frame.height)
-//        detailTextLabel?.frame = CGRect(x: UserData.positionLabel, y: detailTextLabel!.frame.origin.y + 2, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
-//    }
-//    
-//    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-//        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-//        
-//        addSubViews()
-//        setConstriants()
-//    }
-//    
-//    fileprivate func addSubViews() {
-//        addSubview(profileImageView)
-//        addSubview(timeLabel)
-//    }
-//    
-//    fileprivate func setConstriants() {
-//        _ = profileImageView.anchor(nil, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 0, widthConstant: UserData.profileImageSize, heightConstant: UserData.profileImageSize)
-//        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-//        
-//        _ = timeLabel.anchor(topAnchor, left: nil, bottom: nil, right: rightAnchor, topConstant: 20, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 100, heightConstant: 0)
-//    }
-//    
-//    fileprivate func setUserProfile() {
-//        if let id = message?.chatWithSomeone() {
-//            FirebaseDataService.instance.userRef.child(id).observeSingleEvent(of: .value, with: { (snapshot) in
-//                if let value = snapshot.value as? Dictionary<String, AnyObject> {
-//                    self.textLabel?.text = value[Constants.Users.username] as? String
-//                    if let profileImageUrl = value[Constants.Users.imageUrl] as? String {
-//                        self.profileImageView.imageUrlString = profileImageUrl
-//                    }
-//                }
-//            })
-//        }
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
